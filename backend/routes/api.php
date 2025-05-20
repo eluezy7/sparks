a722386a951d8d39a70cbe72196926d221a22974
eluezy7 <gameluzer@gmail.com>
2025-05-20 18:05:41 +0900

    FIX nginx

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,3 +9,7 @@ use App\Http\Controllers\Api\UserInfoController;
 Route::apiResource('user-infos', UserInfoController::class)
      ->only(['index','show','store']);
 Route::get('user-infos', [UserInfoController::class, 'index']);
+
+Route::get('/ping', function () {
+    return response()->json(['message' => 'pong']);
+});
